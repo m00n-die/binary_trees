146980b874e6f944c7bff2b6d628adf47832cb1d
m00n-die <reginaldrock219@gmail.com>
2023-06-02 07:12:48 -0700

    All mandatory tasks

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - Counts the leaves in a binary tree.
+ * @tree: A pointer to the root node of the tree to count the leaves of.
+ *
+ * Return: 0 if tree is NULL
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t leaf = 0;
+
+	if (tree)
+	{
+		leaf += (!tree->left && !tree->right) ? 1 : 0;
+		leaf += binary_tree_leaves(tree->left);
+		leaf += binary_tree_leaves(tree->right);
+	}
+	return (leaf);
+}
---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - Finds the sibling of a
+ *                       node in a binary tree.
+ * @node: A pointer to the node to find the sibling of.
+ *
+ * Return: 0 if tree is NULL
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	return (node->parent->left);
+}
